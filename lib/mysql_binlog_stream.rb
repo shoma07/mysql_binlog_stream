5cf8a96105ca921dd5ecda1b7684391496f9791b
shoma07 <23730734+shoma07@users.noreply.github.com>
2022-09-25 02:19:31 +0900

    initial commit

---
+++ lib/mysql_binlog_stream.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "mysql_binlog_stream/version"
+
+module MysqlBinlogStream
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/mysql_binlog_stream_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe MysqlBinlogStream do
+  it "has a version number" do
+    expect(MysqlBinlogStream::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
